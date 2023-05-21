a6c86edd28ce7b0c7c501cd0c489380bc1afb01d
emilejoelson <emilejoelson@gmail.com>
2023-05-22 00:07:43 +0100

    update all

--- src/main/java/joelsonfatima/dto/response/DriverUserDtoResponse.java
+++ src/main/java/joelsonfatima/dto/response/DriverUserDtoResponse.java
@@ -7,6 +7,11 @@ import java.io.Serializable;
 /**
  * A DTO for the {@link Driver} entity
  */
-public record DriverUserDtoResponse(String cin, String firstName, String lastName, Long userId, String username,
-                                    String roleName) implements Serializable {
+public record DriverUserDtoResponse(
+        String cin,
+        String firstName,
+        String lastName,
+        Long userId,
+        String username,
+        String roleName) implements Serializable {
 }
---
+++ src/test/java/joelsonfatima/MiniProjetJavaTest.java
@@ -0,0 +1,13 @@
+package joelsonfatima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjetJavaTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
